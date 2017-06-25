cd0af1bf8652b615c224f23b16dbd15d41f58722
Vinicius Cardoso <viniciuscardoso@Viniciuss-MacBook-Air.local>
2017-06-25 10:25:29 -0300

    First Commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * versao_git.c
+ *
+ * Created: 6/25/17 9:12:16 AM
+ * Author : viniciuscardoso
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
