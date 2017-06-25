8c9c6d57a6063f5c106efecb1cc4a7e2ab9d87c6
Vinicius Cardoso <viniciuscardoso@Viniciuss-MacBook-Air.local>
2017-06-25 11:19:33 -0300

    First test to read sensor, but was occur an error of communication

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 #include "def_principais.h"
 #include "lcd.h"
+#include "dhtxx.h"
 
 FILE lcd_str = FDEV_SETUP_STREAM(lcd_putchar, NULL, _FDEV_SETUP_WRITE);
 
@@ -19,12 +20,29 @@ int main(void)
 	stdout=&lcd_str;
 	
 	// First Print on Display
+	//cmd_LCD(0x80,0);
+	//printf("Init");
+	
+	// DHT11 Variables
+	unsigned char ec; //Exit code
+	int temp, humid; //Temperature and humidity
+	
 	cmd_LCD(0x80,0);
-	printf("Init");
 	
     while (1) 
     {
+		//Request DHT sensor to give it time to prepare data
+		dhtxxconvert( DHTXX_DHT11, &PORTC, &DDRC, &PINC, ( 1 << 0 ) );
+
+		_delay_ms(2000);
+
+		//Read data from sensor to variables `temp` and `humid` (`ec` is exit code)
+		ec = dhtxxread( DHTXX_DHT11, &PORTC, &DDRC, &PINC, ( 1 << 0 ), &temp, &humid );
 		
+		
+		printf("%d", ec);
+		
+		_delay_ms(1000);
     }
 }
 
