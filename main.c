c8770f970f0a80cd46d3133cb3c0e8b26c6f2f7b
Vinicius Cardoso <viniciuscardoso@Viniciuss-MacBook-Air.local>
2017-06-26 11:00:51 -0300

    Changes with Renan help

--- main.c
+++ main.c
@@ -19,31 +19,35 @@ int main(void)
 	
     uint8_t data [4];
     
-    initDHT();
-    
+
     while(1)
     {
 
-    	fetchData(data);
-    	_delay_ms(100);
+    	uint8_t ret =  fetchData(data);
+    	_delay_ms(1000);
+		
+		
+		 printf("a");
+		 uint8_t check = (data[0] + data[1] + data[2] + data[3]) & 0xFF;
+
+		 try_time = try_time+1;
+		 cmd_LCD(0x80,0);
+		 printf("Umid: %d Try:%d", data[0],try_time);
+		 cmd_LCD(0xC0,0);
+		 printf("Temp: %d", ret);
+		 
+		 //if (check != data[4]) return 0;
+		 
+		 //for(i = 0; i < 4; ++i)
+		 //{ arr[i] = data[i];
+		//	 printf("b");
+		 //}
+		 
+		
 
     }
 
-    
-
     return 0;
-}
-
-void initDHT(void)
-{
-    /* Set LED as output */
-    SET_BIT(DDRB,LED);
-    
-    /* According to the DHT11's datasheet, the sensor needs about
-       1-2 seconds to get ready when first getting power, so we
-       wait
-     */
-    _delay_ms(1000);
 }
 
 
@@ -69,6 +73,9 @@ uint8_t fetchData(uint8_t* arr)
     /* Wait about 20 ms */
     while(TCNT0 < 160);
     
+    //cmd_LCD(0xC0,0);
+//	printf("1");
+
     /* Now set Timer0 with clk/8 prescaling.
      Gives 1탎 per cycle @8Mhz */
     TCCR0B = 0x02;
@@ -85,6 +92,7 @@ uint8_t fetchData(uint8_t* arr)
     while(IS_SET(DHT_PORT_IN,DHT_PIN))
     { if (TCNT0 >= 60) return 0; }
     
+    //printf("2");
     /************************* Sensor preamble *************************/
     
     TCNT0 = 0;
@@ -93,16 +101,18 @@ uint8_t fetchData(uint8_t* arr)
     while(!IS_SET(DHT_PORT_IN,DHT_PIN))
     { if (TCNT0 >= 100) return 0; }
     
+    //printf("3");
     TCNT0 = 0;
     
     /* Then wait for the second response to finish, high ~80탎 */
     while(IS_SET(DHT_PORT_IN,DHT_PIN))
     { if (TCNT0 >= 100) return 0; }
-    
+    //printf("4");
     /********************* Data transmission start **********************/
     
     for (i = 0; i < 5; ++i)
     {
+    	//printf("5");
         for(j = 7; j >= 0; --j)
         {
             TCNT0 = 0;
@@ -110,43 +120,45 @@ uint8_t fetchData(uint8_t* arr)
             /* First there is always a 50탎 low period */
             while(!IS_SET(DHT_PORT_IN,DHT_PIN))
             { if (TCNT0 >= 70) return 0; }
-            
+      //      printf("6");
             TCNT0 = 0;
             
             /* Then the data signal is sent. 26 to 28탎 (ideally)
              indicate a low bit, and around 70탎 a high bit */
             while(IS_SET(DHT_PORT_IN,DHT_PIN))
             { if (TCNT0 >= 90) return 0; }
-            
+      //      printf("7");
             /* Store the value now so that the whole checking doesn't
              move the TCNT0 forward by too much to make the data look
              bad */
             cnt = TCNT0;
-            
+    
+	
+	
+		//	cmd_LCD(0xc0, 0);
+		//	printf("                  ");
+		//	cmd_LCD(0xc0, 0);
+	
+			
+		//	printf("%d", cnt);
+	
+	        
             if (cnt >= 20 && cnt <= 35)
-            { CLEAR_BIT(data[i],j); }
-            
+            { CLEAR_BIT(data[i],j);
+          //  	printf("8");
+            }
             else if (cnt >= 60 && cnt <= 80)
-            { SET_BIT(data[i],j); }
-            
+            { SET_BIT(data[i],j); 
+            //	printf("9");
+            }
             else return 0;
+            
+            //printf("c");
         }
     }
     
     /********************* Sensor communication end *********************/
     
-    check = (data[0] + data[1] + data[2] + data[3]) & 0xFF;
-
-    	try_time = try_time+1;
-		cmd_LCD(0x80,0);
-		printf("Umid: %d Try:%d", data[0],try_time);
-		cmd_LCD(0xC0,0);
-		printf("Temp: %d", data[2]);
-    
-    if (check != data[4]) return 0;
-    
-    for(i = 0; i < 4; ++i)
-    { arr[i] = data[i]; }
     
     return 1;
 }
